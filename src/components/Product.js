8f428fa088bd6c5b03c875709e8b6b830baa22ab
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2021-06-24 01:38:18 +0530

    built checkout page

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
-
+import { useDispatch } from "react-redux";
+import { addItemToBasket, addToBasket } from "../slices/basketSlice";
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 const Product = ({ id, title, price, description, category, image }) => {
+  const dispatch = useDispatch();
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
   const [hasPrime] = useState(Math.random() < 0.5);
-
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime,
+    };
+    //Sending the product as an action to redux store
+    dispatch(addToBasket(product));
+  };
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       {/* Category */}
@@ -52,7 +67,9 @@ const Product = ({ id, title, price, description, category, image }) => {
       )}
 
       {/* Add to Cart */}
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 };
